43025655bb5a4f3a1f652320f09f0c64d29b29eb
SVitaly <user7_490@mail.ru>
2023-02-11 13:53:20 +0300

    добавили решение

--- Program.cs
+++ Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(0, 11);
+        }
+    }
+}
+
+int rows = 5;
+int coloms = 5;
+int[,] Array = new int[rows, coloms];
+FillArray(Array);
+PrintArray(Array);
+
+Console.WriteLine();
+
+int temp;
+
+for (int k = 0; k < Array.GetLength(0); k++)
+{
+    for (int i = 0; i < Array.GetLength(1) - 1; i++)
+    {
+        for (int j = i + 1; j < Array.GetLength(1); j++)
+        {
+            if (Array[k, i] < Array[k, j])
+            {
+                temp = Array[k, i];
+                Array[k, i] = Array[k, j];
+                Array[k, j] = temp;
+            }
+        }
+    }
+}
+
+
+PrintArray(Array);
